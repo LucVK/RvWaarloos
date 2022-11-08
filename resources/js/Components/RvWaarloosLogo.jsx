a2c77f9abe9e750510e0f57f870a75368f20fce9
Luc Van Keer <Luc.VanKeer@riziv-inami.fgov.be>
2022-11-08 18:45:07 +0100

    added:rvmembers

---
+++ resources/js/Components/RvWaarloosLogo.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import logo from '../../images/log-waarloos-300x300.jpg';
+
+export default function RvWaarloosLogo({ className }) {
+    return (
+        <img className={className} src={logo} ></img>
+    );
+}
