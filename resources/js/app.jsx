19d19fee6d85185212c67e7c742ffde805878174
Luc Van Keer <Luc.VanKeer@riziv-inami.fgov.be>
2022-11-12 08:43:09 +0100

    saved

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -9,6 +9,25 @@ import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 
 import { registerLicense } from '@syncfusion/ej2-base';
 
+import * as ej2localenl from '@syncfusion/ej2-locale/src/nl.json'
+import * as numbers from 'cldr-data/main/nl/numbers.json';
+import * as datefields from 'cldr-data/main/nl/datefields.json';
+import * as gregorian from 'cldr-data/main/nl/ca-gregorian.json';
+import * as timeZoneNames from 'cldr-data/main/nl/timeZoneNames.json';
+
+
+import { loadCldr } from '@syncfusion/ej2-base';
+
+import {L10n, setCulture} from '@syncfusion/ej2-base';
+
+loadCldr(numbers, gregorian, timeZoneNames,datefields);
+
+setCulture('nl');
+
+L10n.load(ej2localenl);
+
+
+
 // Registering Syncfusion license key
 registerLicense('ORg4AjUWIQA/Gnt2VVhjQlFaclhJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRd0RjXn5edHxQQmBcVE0=');
 
